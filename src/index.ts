95c2483f430716056e66340d1575d70be8655620
Rikieri Sartor <riki_eri@hotmail.com>
2021-04-11 21:50:58 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+  https://docs.aws.amazon.com/pt_br/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html
+  
+  Idea: 
+    - init
+    - setItem
+    - getItem
+    - removeItem
+
+  - createBucket?
+  - getBucket?
+*/
